b4c0fcfc8475c2f4946dd99104741e4d0cbc7383
mirvarimuradova <mirvarimuradova7@gmail.com>
2023-11-29 16:40:49 +0400

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './conponent/About';
+import Contact from './conponent/Contact';
+import Examples from './conponent/Examples';
+import Home from './conponent/Home';
 
-function App() {
+  const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+      
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li> <li>
+              <Link to="/about">Haqqında</Link>
+            </li>
+            <li>
+              <Link to="/examples">İş nümunələri</Link>
+            </li>
+            <li>
+              <Link to="/contact">Əlaqə məlumatları</Link>
+            </li>
+          </ul>
+    
+
+        <hr />
+         <Routes>
+        <Route  path="/" element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path="/examples" element={<Examples/>} />
+        <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    
   );
-}
+};
 
 export default App;
